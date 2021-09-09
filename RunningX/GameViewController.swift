691ee16f770c4a9208c9ddb6c82bcf9f55b1f53e
Anderson Sprenger <anderson.sprenger@edu.pucrs.br>
2021-09-09 17:44:45 -0300

    cenário ok

--- RunningX/GameViewController.swift
+++ RunningX/GameViewController.swift
@@ -9,13 +9,15 @@ import UIKit
 import QuartzCore
 import SceneKit
 
-class GameViewController: UIViewController {
-
+class GameViewController: UIViewController, SCNSceneRendererDelegate {
+    
+    // MARK: -- Init
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
         
         // create and add a camera to the scene
         let cameraNode = SCNNode()
@@ -23,7 +25,7 @@ class GameViewController: UIViewController {
         scene.rootNode.addChildNode(cameraNode)
         
         // place the camera
-        cameraNode.position = SCNVector3(x: 0, y: 0, z: 15)
+        cameraNode.position = SCNVector3(x: 0, y: 5, z: 15)
         
         // create and add a light to the scene
         let lightNode = SCNNode()
@@ -39,12 +41,6 @@ class GameViewController: UIViewController {
         ambientLightNode.light!.color = UIColor.darkGray
         scene.rootNode.addChildNode(ambientLightNode)
         
-        // retrieve the ship node
-        let ship = scene.rootNode.childNode(withName: "ship", recursively: true)!
-        
-        // animate the 3d object
-        ship.runAction(SCNAction.repeatForever(SCNAction.rotateBy(x: 0, y: 2, z: 0, duration: 1)))
-        
         // retrieve the SCNView
         let scnView = self.view as! SCNView
         
@@ -52,54 +48,74 @@ class GameViewController: UIViewController {
         scnView.scene = scene
         
         // allows the user to manipulate the camera
-        scnView.allowsCameraControl = true
+        scnView.allowsCameraControl = false
         
         // show statistics such as fps and timing information
         scnView.showsStatistics = true
         
         // configure the view
-        scnView.backgroundColor = UIColor.black
+        scnView.backgroundColor = UIColor.systemBlue
         
-        // add a tap gesture recognizer
-        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap(_:)))
-        scnView.addGestureRecognizer(tapGesture)
+        scnView.delegate = self
+        
+        createScenary()
     }
     
-    @objc
-    func handleTap(_ gestureRecognize: UIGestureRecognizer) {
-        // retrieve the SCNView
-        let scnView = self.view as! SCNView
+    // MARK: -- Update
+    
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        moveScenrary()
+    }
+    
+    // MARK: -- Functions
+    
+    func createScenary() {
+        let nodeLength: CGFloat = 20
         
-        // check what nodes are tapped
-        let p = gestureRecognize.location(in: scnView)
-        let hitResults = scnView.hitTest(p, options: [:])
-        // check that we clicked on at least one object
-        if hitResults.count > 0 {
-            // retrieved the first clicked object
-            let result = hitResults[0]
+        for i in 0..<7 {
+            let ground = SCNNode(geometry: SCNBox(width: 20, height: 1, length: nodeLength, chamferRadius: 0.0))
+            ground.position = SCNVector3(x: 0, y: 0, z: 0)
+            ground.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "grass")
             
-            // get its material
-            let material = result.node.geometry!.firstMaterial!
+            let left = SCNNode(geometry: SCNBox(width: 5, height: 10, length: nodeLength, chamferRadius: 0.0))
+            left.position = SCNVector3(x: -10, y: 5, z: 0)
+            left.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "florest")
             
-            // highlight it
-            SCNTransaction.begin()
-            SCNTransaction.animationDuration = 0.5
+            let right = SCNNode(geometry: SCNBox(width: 5, height: 10, length: nodeLength, chamferRadius: 0.0))
+            right.position = SCNVector3(x: 10, y: 5, z: 0)
+            right.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "florest")
             
-            // on completion - unhighlight
-            SCNTransaction.completionBlock = {
-                SCNTransaction.begin()
-                SCNTransaction.animationDuration = 0.5
-                
-                material.emission.contents = UIColor.black
-                
-                SCNTransaction.commit()
-            }
+            let node = SCNNode()
+            node.name = "ground"
+            node.addChildNode(ground)
+            node.addChildNode(left)
+            node.addChildNode(right)
+            node.position = SCNVector3(x: 0, y: 0, z: Float(i) * -20)
             
-            material.emission.contents = UIColor.red
-            
-            SCNTransaction.commit()
+            let scnView = self.view as! SCNView
+            scnView.scene?.rootNode.addChildNode(node)
         }
     }
+    
+    func moveScenrary() {
+        DispatchQueue.main.async {
+            let scnView = self.view as! SCNView
+            
+            let nodes = scnView.scene?.rootNode.childNodes.filter { node in
+                node.name == "ground"
+            }
+            
+            for node in nodes! {
+                node.position.z += 0.2
+                
+                if node.position.z > 20 {
+                    node.position.z -= 120
+                }
+            }
+        }
+    }
+    
+    // MARK: -- Configs
     
     override var shouldAutorotate: Bool {
         return true
@@ -116,5 +132,4 @@ class GameViewController: UIViewController {
             return .all
         }
     }
-
 }
